0c4bdcdc5e467eb2f970360dd834cf39bc213325
YJ <yj@YJMacBook-Pro.local>
2025-02-01 20:41:10 +0900

    refactoring bookmark toggle

--- src/js/bookmarkToggle.js
+++ src/js/bookmarkToggle.js
@@ -1,40 +1,46 @@
 const $bookmarkBar = document.getElementById("bookmark-bar");
-const $openBtn = document.getElementById("bookmark-open-btn");
-const $closeBtn = document.getElementById("bookmark-close-btn");
-const $openText = document.getElementById("bookmark-open-btn-text");
-const $closeText = document.getElementById("bookmark-close-btn-text");
+const $bookmarkButton = document.getElementById("bookmark-button");
 
 const currentBookmarkBarState = localStorage.getItem("currentBookmarkBarState");
 
+if (currentBookmarkBarState === "open") {
+  $bookmarkBar.style.display = "block";
+  $bookmarkButton.className = "btn-open";
+  $bookmarkButton.textContent = "북마크 닫기";
+} else {
+  $bookmarkBar.style.display = "none";
+  $bookmarkButton.className = "btn-close";
+  $bookmarkButton.textContent = "북마크 열기";
+}
+
 const bookmarkBarToggle = () => {
   let currenState = localStorage.getItem("currentBookmarkBarState");
+
+  // localStorage에 currentBookmarkBarState 값이 없을 경우
+  if (!currenState) {
+    localStorage.setItem("currentBookmarkBarState", "open");
+    $bookmarkBar.style.display = "block";
+    $bookmarkButton.className = "btn-open";
+    $bookmarkButton.textContent = "북마크 닫기";
+    return;
+  }
 
   // localStorage에 currentBookmarkBarState 값이 존재하고 상태가 close일 경우
   // close -> open
   if (currenState === "close") {
     localStorage.setItem("currentBookmarkBarState", "open");
     $bookmarkBar.style.display = "block";
-    $closeBtn.style.display = "flex";
-    $openBtn.style.display = "none";
+    $bookmarkButton.className = "btn-open";
+    $bookmarkButton.textContent = "북마크 닫기";
     return;
   }
-  // localStorage에 currentBookmarkBarState 값이 없거나 상태가 open일 경우
+
+  // localStorage에 currentBookmarkBarState 값이 존재하고 상태가 open일 경우
   // open -> close
   localStorage.setItem("currentBookmarkBarState", "close");
   $bookmarkBar.style.display = "none";
-  $closeBtn.style.display = "none";
-  $openBtn.style.display = "flex";
+  $bookmarkButton.className = "btn-close";
+  $bookmarkButton.textContent = "북마크 열기";
 };
 
-if (currentBookmarkBarState === "open") {
-  $bookmarkBar.style.display = "block";
-  $closeBtn.style.display = "flex";
-  $openBtn.style.display = "none";
-} else {
-  $bookmarkBar.style.display = "none";
-  $closeBtn.style.display = "none";
-  $openBtn.style.display = "flex";
-}
-
-$openBtn.addEventListener("click", bookmarkBarToggle);
-$closeBtn.addEventListener("click", bookmarkBarToggle);
+$bookmarkButton.addEventListener("click", bookmarkBarToggle);
