22872f9a2611bf364160da4764e74126973a4ceb
YJ <yj@YJMacBook-Pro.local>
2025-02-03 09:42:53 +0900

    add manifest.json & update README.md

--- src/js/search.js
+++ src/js/search.js
@@ -2,9 +2,8 @@ const $search = document.getElementById("search-input");
 
 const moveResultPage = () => {
   let searchWord = $search.value;
-
+  $search.value = "";
   window.location.href = `https://google.com/search?q=${searchWord}`;
-  searchWord = "";
 };
 
 const onEnter = (eCode) => {
